d06a52512ad05f9eb98c90fd70c3d1086f24648d
kevingao26 <70555752+kevingao26@users.noreply.github.com>
2022-08-11 17:34:04 -0700

    Add files via upload

---
+++ zot2.sh
@@ -0,0 +1,10 @@
+url="$1"
+
+# zotero translator server must be open first with ?
+# use translator to get Zotero API JSON for URL
+zjson=$(curl -d "$1"   -H 'Content-Type: text/plain' http://127.0.0.1:1969/search)
+echo $zjson > temp1.json
+
+# convert this JSON to supported import format, eg. Bibtex
+bib=$(curl -d @temp1.json -H 'Content-Type: application/json' 'http://127.0.0.1:1969/export?format=bibtex')
+echo $bib > temp2.json
